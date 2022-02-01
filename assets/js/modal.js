2181cf8edff6031ed9948992862a7a60baa5dcd0
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2022-02-01 12:42:49 +0100

    Photo modals

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -69,6 +69,74 @@ function init(){
             enableScroll();
         }
     }
+    if(document.getElementById("img5")){
+        let img5, modal5, modalCloser5;
+
+        img5 = document.getElementById("img5");
+        modal5 = document.getElementById("modal5");
+        modalCloser5 = document.getElementById("modal5-closer");
+        img5.onclick = function(){
+            modalContainer.setAttribute("style", "display: flex;");
+            modal5.setAttribute("style", "display: block;");
+            disableScroll();
+        };
+        modalCloser5.onclick = function(){
+            modal5.setAttribute("style", "display: none;");
+            modalContainer.setAttribute("style", "display: none;");
+            enableScroll();
+        }
+    }
+    if(document.getElementById("img6")){
+        let img6, modal6, modalCloser6;
+
+        img6 = document.getElementById("img6");
+        modal6 = document.getElementById("modal6");
+        modalCloser6 = document.getElementById("modal6-closer");
+        img6.onclick = function(){
+            modalContainer.setAttribute("style", "display: flex;");
+            modal6.setAttribute("style", "display: block;");
+            disableScroll();
+        };
+        modalCloser6.onclick = function(){
+            modal6.setAttribute("style", "display: none;");
+            modalContainer.setAttribute("style", "display: none;");
+            enableScroll();
+        }
+    }
+    if(document.getElementById("img7")){
+        let img7, modal7, modalCloser7;
+
+        img7 = document.getElementById("img7");
+        modal7 = document.getElementById("modal7");
+        modalCloser7 = document.getElementById("modal7-closer");
+        img7.onclick = function(){
+            modalContainer.setAttribute("style", "display: flex;");
+            modal7.setAttribute("style", "display: block;");
+            disableScroll();
+        };
+        modalCloser7.onclick = function(){
+            modal7.setAttribute("style", "display: none;");
+            modalContainer.setAttribute("style", "display: none;");
+            enableScroll();
+        }
+    }
+    if(document.getElementById("img8")){
+        let img8, modal8, modalCloser8;
+
+        img8 = document.getElementById("img8");
+        modal8 = document.getElementById("modal8");
+        modalCloser8 = document.getElementById("modal8-closer");
+        img8.onclick = function(){
+            modalContainer.setAttribute("style", "display: flex;");
+            modal8.setAttribute("style", "display: block;");
+            disableScroll();
+        };
+        modalCloser8.onclick = function(){
+            modal8.setAttribute("style", "display: none;");
+            modalContainer.setAttribute("style", "display: none;");
+            enableScroll();
+        }
+    }
 }
 
 function preventDefault(e) {
